216e2865e8a237f6ee44ade92892f84052fc65df
Yanina Bustos <yanibustos4596@gmail.com>
2025-03-25 17:27:44 -0300

    Layout - Add Navbar, Outlet and Footer

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,18 @@
+import { Outlet } from "react-router-dom";
+
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+
 import "./Layout.css";
 
 function Layout() {
-  return <div className="layout-container">Layout</div>;
+  return (
+    <div className="layout-container">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
 
 export default Layout;
