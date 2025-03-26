950245c334d5c55ffe4e9a95ce91622b7a725362
ayhesahinds <ayhesahinds@gmail.com>
2025-03-26 00:32:20 -0300

    Update Navbar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
 
-import Navbar from "../Navbar/Navbar";
+import NavMenu from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 
+
 import "./Layout.css";
+
 
 function Layout() {
   return (
     <div className="layout-container">
-      <Navbar />
+      <NavMenu />
+      
       <Outlet />
       <Footer />
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,41 @@
+import React from "react";
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
 import "./Navbar.css";
 
-function Navbar() {
-  return <div className="navbar-container">Navbar</div>;
+
+
+function NavMenu() {
+
+  return <div className="navbar-container fixed-top">
+    <Navbar collapseOnSelect expand="lg" >
+      <Container className="gx-1">
+        <Navbar.Brand className="mb-2 logo" href="#home" ><img src="../img/logo-white.png" alt="Home-Deluxe"></img></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link className="nav-link" href="#Catalog">Catalog</Nav.Link>
+            <Nav.Link className="nav-link" href="#Featured">Featured</Nav.Link>
+            <Nav.Link className="nav-link" href="#AboutUs">About us</Nav.Link>
+
+          </Nav>
+          <Nav>
+            <Nav.Link className="nav-font" href="#deets">My Account</Nav.Link>
+            <Nav.Link className="nav-font" eventKey={2} href="#memes">
+              logocarrito*
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+   
+  </div>;
+  
 }
 
-export default Navbar;
+export default NavMenu;
