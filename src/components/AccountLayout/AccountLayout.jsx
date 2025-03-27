b3c86efb656d4780a9dfe7a3bb5897f3713dc776
Yanina Bustos <yanibustos4596@gmail.com>
2025-03-27 13:44:57 -0300

    AccountLayout - Add Outlet

--- src/components/AccountLayout/AccountLayout.jsx
+++ src/components/AccountLayout/AccountLayout.jsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
 import "./AccountLayout.css";
 
 function AccountLayout() {
-  return <div className="accountLayout-container">AccountLayout</div>;
+  return (
+    <div className="accountLayout-container">
+      AccountLayout
+      <Outlet />
+    </div>
+  );
 }
 
 export default AccountLayout;
