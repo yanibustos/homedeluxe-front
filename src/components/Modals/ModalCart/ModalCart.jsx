3009c7d842adb19d85800a0f81c16f415cb0f7e0
Darlen Hornia Puentes <hornia84@gmail.com>
2025-03-27 16:56:04 -0300

    ModalCart - Add component

--- src/components/Modals/ModalCart/ModalCart.jsx
+++ src/components/Modals/ModalCart/ModalCart.jsx
@@ -1,7 +1,102 @@
+import { useState } from "react";
+import { Modal } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+import { removeFromcart } from "../../../redux/shoppingCartSlice";
+import CloseModalBtn from "./CloseModalBtn";
+
 import "./ModalCart.css";
+import ProductCartQty from "./ProductCartQty";
 
 function ModalCart() {
-  return <div className="modalCart-container">ModalCart</div>;
+  const shoppingCart = useSelector((state) => state.shoppingCart);
+  const [show, setShow] = useState(true);
+  const dispatch = useDispatch();
+  const isEmpty = shoppingCart.length === 0;
+
+  const handleClose = () => setShow(false);
+
+  const handleRemove = (productId) => {
+    dispatch(removeFromcart(productId));
+  };
+
+  const formatPrice = (value) => {
+    return value.toLocaleString("es-ES", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+  };
+
+  const totalPrice = formatPrice(
+    shoppingCart.reduce((total, product) => total + product.price * product.quantity, 0),
+  );
+
+  return (
+    <div className="modalCart-container bg-dark">
+      <Modal show={show} onHide={handleClose} fullscreen="sm-down" size={"md"}>
+        <Modal.Body className="d-flex justify-content-center">
+          {!isEmpty ? (
+            <div className="details w-100 d-flex flex-column">
+              <div className="container header">
+                <div className="row fw-bold pb-2">
+                  <div className="col-7 text-uppercase ps-0">Articles ({shoppingCart.length})</div>
+                  <div className="col-2 text-uppercase qty text-center">Qty. </div>
+                  <div className="col-2 text-uppercase price text-end">Price </div>
+                </div>
+              </div>
+              <div className="container content position-relative">
+                {shoppingCart.map((item) => (
+                  <div className="row product d-flex" key={item.id}>
+                    <div className="col-7 px-0">
+                      <Link to={`products/${item.slug}`} className="text-decoration-none d-flex">
+                        <img src={item.image} alt={item.name} className="me-2" />
+                        <div>
+                          <p className="mb-0 fw-bold">{item.name}</p>
+                          <p className="sku">{item.sku}</p>
+                        </div>
+                      </Link>
+                    </div>
+                    <div className="col-2 qty-wrapper text-center px-0">
+                      <ProductCartQty product={item} />
+                    </div>
+                    <div className="col-2 text-center px-0 d-flex">
+                      <span className="price-value d-flex me-2">
+                        <span className="currency text-uppercase">usd</span>{" "}
+                        <span>{formatPrice(item.price * item.quantity)}</span>
+                      </span>
+                      <span className="ico-trash small" onClick={() => handleRemove(item.id)}>
+                        Del
+                      </span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="footer mt-auto">
+                <div className="amount d-flex flex-column justify-content-center align-items-center position-relative pt-3">
+                  <p className="fw-bold mb-3">
+                    Order Total: <span className="currency text-uppercase">usd </span>
+                    {totalPrice}
+                  </p>
+                  <Link to="/checkout" className="btn-standard btn01 text-decoration-none">
+                    Complete Purchase
+                  </Link>
+                  <CloseModalBtn handleClose={handleClose} />
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="d-flex flex-column">
+              <p id="empty-cart" className="m-0 p-0">
+                You haven’t added any products yet.
+              </p>
+              <CloseModalBtn handleClose={handleClose} />
+            </div>
+          )}
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
 }
 
 export default ModalCart;
