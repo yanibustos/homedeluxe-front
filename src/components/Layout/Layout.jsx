0ede7238f5f9f53758e1bbe32ea91047979cf1e5
ayhesahinds <ayhesahinds@gmail.com>
2025-03-27 18:56:02 -0300

    Revert "Update Navbar"
    This reverts commit 950245c334d5c55ffe4e9a95ce91622b7a725362.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,17 +1,14 @@
 import { Outlet } from "react-router-dom";
 
-import NavMenu from "../Navbar/Navbar";
+import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 
-
 import "./Layout.css";
-
 
 function Layout() {
   return (
     <div className="layout-container">
-      <NavMenu />
-      
+      <Navbar />
       <Outlet />
       <Footer />
     </div>
