e82f6e0856eeeb030cd7419743e876d028c4fb63
Darlen Hornia Puentes <hornia84@gmail.com>
2025-03-27 23:42:15 -0300

    Hero - Add component to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,13 @@
+import Hero from "../../components/Hero";
+
 import "./Home.css";
 
 function Home() {
-  return <div className="home-container">Home</div>;
+  return (
+    <div className="home-container">
+      <Hero />
+    </div>
+  );
 }
 
 export default Home;
