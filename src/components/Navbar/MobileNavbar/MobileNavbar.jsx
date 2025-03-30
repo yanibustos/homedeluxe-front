76f03aa427dd0c7c7bcc1a3a5c12ab2cf3a0b42f
Darlen Hornia Puentes <hornia84@gmail.com>
2025-03-30 19:06:22 -0300

    MobileNavbar - Add user dropdown menu

--- src/components/Navbar/MobileNavbar/MobileNavbar.jsx
+++ src/components/Navbar/MobileNavbar/MobileNavbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 import CartIcon from "../../commons/CartIcon/CartIcon";
@@ -14,7 +14,9 @@ const MobileNavbar = () => {
   const user = useSelector((state) => state.user);
   const [show, setShow] = useState(false);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [showAccount, setShowAccount] = useState(false);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleShowModal = () => {
     setShow(true);
@@ -27,6 +29,10 @@ const MobileNavbar = () => {
   const signOut = () => {
     setIsSidebarOpen(false);
     dispatch(logout());
+  };
+
+  const toggleAccount = () => {
+    setShowAccount(!showAccount);
   };
 
   return (
@@ -44,31 +50,60 @@ const MobileNavbar = () => {
           <img className="logo-img" src="../../img/logo-white.png" alt="Home Deluxe" />
         </Link>
       </div>
-
+      <div
+        className={`offcanvas-backdrop ${isSidebarOpen ? "show" : "d-none"}`}
+        onClick={toggleSidebar}
+      ></div>
       <div
         className={`offcanvas offcanvas-start d-flex ${isSidebarOpen ? "show" : "hide"}`}
         tabIndex="-1"
         id="offcanvasNavbar"
         aria-labelledby="offcanvasNavbarLabel"
       >
-        <div className="offcanvas-header">
-          <button
-            type="button"
-            className="btn-close text-reset"
-            onClick={toggleSidebar}
-            aria-label="Close"
-          ></button>
-        </div>
         <div className="offcanvas-body">
           <ul className="navbar-nav justify-content-end flex-grow-1">
-            <li className="nav-item">
-              <Link className="nav-link my-account d-flex" to="/profile">
-                <span className="user-icon-wrapper me-2">
-                  <i className="bi bi-person-fill fs-5"></i>
-                </span>
-                My Account
-              </Link>
+            <li className="nav-item d-flex">
+              <div className="nav-link my-account d-flex flex-column p-0">
+                <button
+                  type="button"
+                  className="btn btn-link nav-link my-account d-flex"
+                  onClick={() => {
+                    !user.accessToken ? navigate("/profile") : toggleAccount();
+                  }}
+                >
+                  <span className="user-icon-wrapper me-2">
+                    <i className="bi bi-person-fill fs-5"></i>
+                  </span>
+                  {user.accessToken ? `${user.firstname} ${user.lastname}` : "My Account"}
+                  {user.accessToken && (
+                    <div className="btnItem" data-show={showAccount ? "on" : ""}></div>
+                  )}
+                </button>
+                {user.accessToken && showAccount && (
+                  <div id="collapseOne" className="collapse show" aria-labelledby="headingOne">
+                    <div>
+                      <Link to="/profile" className="nav-link" onClick={toggleSidebar}>
+                        My Profile
+                      </Link>
+                      <Link to="/orders" className="nav-link" onClick={toggleSidebar}>
+                        My Orders
+                      </Link>
+                      <Link
+                        className="nav-link"
+                        to="/"
+                        onClick={() => {
+                          signOut();
+                          toggleSidebar();
+                        }}
+                      >
+                        Logout
+                      </Link>
+                    </div>
+                  </div>
+                )}
+              </div>
             </li>
+
             <li className="nav-item">
               <Link className="nav-link" to="/products" onClick={toggleSidebar}>
                 Shop
@@ -84,23 +119,13 @@ const MobileNavbar = () => {
                 About this project
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/profile" onClick={toggleSidebar}>
-                Profile
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/" onClick={signOut}>
-                Logout
-              </Link>
-            </li>
           </ul>
         </div>
       </div>
 
       <div className="cart-icon" onClick={handleShowModal}>
         <CartIcon />
-        <span className="cart-qty">{totalQty(shoppingCart)}</span>
+        {shoppingCart.length !== 0 && <span className="cart-qty">{totalQty(shoppingCart)}</span>}
       </div>
       <ModalCart show={show} setShow={setShow} />
     </div>
