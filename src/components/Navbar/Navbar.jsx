27708b8b13cbdc70c4b703496e380ad55afd5397
Darlen Hornia Puentes <hornia84@gmail.com>
2025-03-30 19:07:22 -0300

    Navbar - Fix User dropdown menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 import CartIcon from "../commons/CartIcon/CartIcon";
@@ -17,11 +17,13 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 function Navbar() {
   const [show, setShow] = useState(false);
   const shoppingCart = useSelector((state) => state.shoppingCart);
+  const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const { pathname } = useLocation();
   const isHomePath = pathname === "/";
   const navRef = useRef(null);
   const isMobile = useMediaQuery("(max-width: 992px)");
+  const navigate = useNavigate();
 
   const handleShowModal = () => {
     setShow(true);
@@ -95,43 +97,50 @@ function Navbar() {
                 className="btn btn-transparent dropdown-toggle nav-item"
                 type="button"
                 id="dropdownMenuButton"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
+                data-bs-toggle={user.accessToken ? "dropdown" : ""}
+                aria-expanded={user.accessToken ? "true" : "false"}
+                onClick={() => (!user.accessToken ? navigate("/login") : {})}
               >
-                My account
+                {user.accessToken ? `${user.firstname} ${user.lastname}` : "My Account"}
               </button>
-              <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <li>
-                  <Link className="dropdown-item" to="/profile">
-                    Profile
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/orders">
-                    Orders
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/" onClick={signOut}>
-                    Logout
-                  </Link>
-                </li>
-              </ul>
+              {user.accessToken && (
+                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <li>
+                    <Link className="dropdown-item" to="/profile">
+                      Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/orders">
+                      Orders
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/" onClick={signOut}>
+                      Logout
+                    </Link>
+                  </li>
+                </ul>
+              )}
             </li>
 
             <li className="nav-item d-flex">
               <span className="nav-link" onClick={handleShowModal}>
                 <span className="position-relative">
                   <CartIcon />
-                  <span className="position-absolute top-0 start-0 cart-qty">
-                    {totalQty(shoppingCart)}
-                    <span className="visually-hidden">products quantity</span>
+                  {shoppingCart.length !== 0 && (
+                    <span className="position-absolute top-0 start-0 cart-qty">
+                      {totalQty(shoppingCart)}
+                      <span className="visually-hidden">products quantity</span>
+                    </span>
+                  )}
+                </span>
+                {shoppingCart.length !== 0 && (
+                  <span className="total-price-wrapper">
+                    <span className="ms-2 currency text-uppercase">usd</span>
+                    <span className="ms-1">{totalPrice(shoppingCart)}</span>
                   </span>
-                </span>
-                <span className="total-price-wrapper">
-                  <span className="ms-2 currency text-uppercase">usd</span>
-                  <span className="ms-1">{totalPrice(shoppingCart)}</span>
-                </span>
+                )}
               </span>
 
               <span className="me-2">
