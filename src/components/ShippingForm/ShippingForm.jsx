e93fac2d2a12d3baaa9aae516cfc7be8aba7943c
FedeVargas21 <fedevargass00@gmail.com>
2025-03-31 12:08:38 -0300

    ShippingForm - Add content and styles - Component JSX

---
+++ src/components/ShippingForm/ShippingForm.jsx
@@ -0,0 +1,133 @@
+import React from "react";
+
+const ShippingForm = ({ formData, handleChange }) => {
+  return (
+    <div>
+      <h4 className="mb-4">Shipping Information</h4>
+      <hr className="my-4" />
+      <div className="row">
+        <div className="col-md-6 mb-3">
+          <label htmlFor="firstName" className="form-label">
+            First Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="col-md-6 mb-3">
+          <label htmlFor="lastName" className="form-label">
+            Last Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="email" className="form-label">
+          Email Address
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+
+      <div className="row">
+        <div className="col-md-6 mb-3">
+          <label htmlFor="country" className="form-label">
+            Country
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="country"
+            name="country"
+            value={formData.country}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="col-md-6 mb-3">
+          <label htmlFor="city" className="form-label">
+            City
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="city"
+            name="city"
+            value={formData.city}
+            onChange={handleChange}
+            required
+          />
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-6 mb-3">
+          <label htmlFor="phone" className="form-label">
+            Phone Number
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="col-md-6 mb-3">
+          <label htmlFor="zip" className="form-label">
+            Zip Code
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="zip"
+            name="zip"
+            value={formData.zip}
+            onChange={handleChange}
+            required
+          />
+        </div>
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="address" className="form-label">
+          Address
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="address"
+          name="address"
+          value={formData.address}
+          onChange={handleChange}
+          required
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ShippingForm;
