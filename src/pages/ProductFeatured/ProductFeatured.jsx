14226759926afa163429b7aef011663f9ab650e8
Yanina Bustos <yanibustos4596@gmail.com>
2025-04-02 04:07:16 -0300

    Wishlist - Add incoming message

--- src/pages/ProductFeatured/ProductFeatured.jsx
+++ src/pages/ProductFeatured/ProductFeatured.jsx
@@ -3,7 +3,9 @@ import "./ProductFeatured.css";
 function ProductFeatured() {
   return (
     <div className="productFeatured-container container d-flex align-items-center justify-content-center">
-      <h1>Featured products will be available soon, stay tuned for updates!</h1>
+      <h1 className="text-center">
+        Featured products will be available soon, stay tuned for updates!
+      </h1>
     </div>
   );
 }
