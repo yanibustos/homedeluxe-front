b8fab3453776c123bdf9cdd4089f1be5129c1245
Yanina Bustos <yanibustos4596@gmail.com>
2025-04-01 21:37:37 -0300

    Login - Connect Login to DB

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,17 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const testUser = {
-  id: 1,
-  firstname: "John",
-  lastname: "Doe",
-  email: "johndoe@example.com",
-  accessToken: "fake-jwt-token-123456789",
-  avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsd6jEYWceiGnd-fmcfMQU81ME5zJj63buBw&s",
-};
+//Uncomment to test without DB
+// const testUser = {
+//   id: 1,
+//   firstname: "John",
+//   lastname: "Doe",
+//   email: "johndoe@example.com",
+//   accessToken: "fake-jwt-token-123456789",
+//   avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsd6jEYWceiGnd-fmcfMQU81ME5zJj63buBw&s",
+// };
 
 const userSlice = createSlice({
   name: "user",
-  initialState: testUser,
+  initialState: {},
   reducers: {
     login(state, action) {
       return action.payload;
