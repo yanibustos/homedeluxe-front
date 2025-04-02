76ebc1b0736a6a2cca68714e6f7efd4796c9f736
Darlen Hornia Puentes <hornia84@gmail.com>
2025-04-02 01:50:36 -0300

    Hero - Add text and CTA

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay, EffectFade } from "swiper/modules";
 import { Link } from "react-router-dom";
@@ -13,46 +13,67 @@ import "./Hero.css";
 const slides = [
   {
     id: 1,
-    image: "./img/hero/slide1.png",
+    image: "./img/hero/slide1.webp",
     url: "/products",
+    caption: "Transform your home, transform your life",
   },
   {
     id: 2,
-    image: "./img/hero/slide2.png",
+    image: "./img/hero/slide2.webp",
     url: "/products",
+    caption: "Sweet Dreams Begin with the Perfect Bed!",
   },
   {
     id: 3,
-    image: "./img/hero/slide3.png",
+    image: "./img/hero/slide3.webp",
     url: "/products",
+    caption: "Now More Affordable Than Ever.",
   },
   {
-    id: 3,
-    image: "./img/hero/slide4.png",
+    id: 4,
+    image: "./img/hero/slide4.webp",
     url: "/products",
+    caption: "Dine in Comfort, Live in Elegance",
   },
 ];
 
 function Hero() {
+  const [captionVisible, setCaptionVisible] = useState(false);
+
+  const handleSlideChange = () => {
+    setCaptionVisible(false);
+    setTimeout(() => setCaptionVisible(true), 500);
+  };
+
   return (
     <div className="home-swipper">
       <Swiper
         modules={[Pagination, Autoplay, EffectFade]}
         pagination={{ clickable: true }}
-        loop={true}
+        loop
         spaceBetween={50}
         slidesPerView={1}
+        slidesPerGroup={1}
         autoplay={{
           delay: 5000,
         }}
         effect="fade"
         speed={2000}
+        onSlideChange={handleSlideChange}
       >
         {slides.map((slide) => (
           <SwiperSlide key={slide.id}>
-            <Link to={slide.url}>
-              <img src={slide.image} alt={`Slide ${slide.id}`} />
-            </Link>
+            <div className="slide-content position-relative">
+              <Link to={slide.url}>
+                <img src={slide.image} alt={`Slide ${slide.id}`} />
+              </Link>
+              <div className={`caption-container ${captionVisible ? "visible" : ""}`}>
+                <div className="slide-caption">{slide.caption}</div>
+                <Link to={slide.url}>
+                  <button className="slide-button">Shop Now</button>
+                </Link>
+              </div>
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
