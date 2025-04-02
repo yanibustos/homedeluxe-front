f65ea75ac4405ff70b4e40a3ed5410fc7772cbc1
Yanina Bustos <yanibustos4596@gmail.com>
2025-04-02 03:49:09 -0300

    ProductFeatured - Add incoming content message

--- src/pages/ProductFeatured/ProductFeatured.jsx
+++ src/pages/ProductFeatured/ProductFeatured.jsx
@@ -1,7 +1,11 @@
 import "./ProductFeatured.css";
 
 function ProductFeatured() {
-  return <div className="productFeatured-container">ProductFeatured</div>;
+  return (
+    <div className="productFeatured-container container d-flex align-items-center justify-content-center">
+      <h1>Featured products will be available soon, stay tuned for updates!</h1>
+    </div>
+  );
 }
 
 export default ProductFeatured;
