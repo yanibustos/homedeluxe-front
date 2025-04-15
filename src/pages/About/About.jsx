049cfe494a47c3e9259d8994b6eeb27aa0f747c2
FedeVargas21 <fedevargass00@gmail.com>
2025-04-15 23:56:54 +0200

    About - Solved responsive problems in sections 2 and 3

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -70,7 +70,7 @@ const About = () => {
   return (
     <div className="about-container">
       <div className="about-section section1">
-        <div className="d-flex justify-content-center align-items-start flex-column">
+        <div className="container d-flex justify-content-center align-items-start flex-column">
           <div className="text-center">
             <h1>About This Project</h1>
             <p>
@@ -86,41 +86,43 @@ const About = () => {
       </div>
 
       <div className="section2 d-flex">
-        <div className="section2-text w-75">
-          <h2>Technologies & Tools</h2>
-          <p>
-            Throughout the bootcamp, we explored various backend and frontend technologies while
-            gaining practical experience with databases and development tools.
-          </p>
-          <br></br>
-          <p>
-            To ensure an organized and efficient workflow, we used Trello for task management and
-            GitHub for collaboration across three defined sprints. Discord served as our main
-            communication channel, helping us coordinate effectively as a team.
-          </p>
-          <p>
-            These tools played a key role in enhancing our productivity and maintaining a structured
-            development process.
-          </p>
-        </div>
-        <div className="icons w-75">
-          <div className="technologies-row d-flex flex-wrap">
-            {techTools.map((item, index) => (
-              <div className="tecnology-item col-3 text-center py-4" key={index}>
-                {item.isCustomIcon ? (
-                  <SupabaseIcon className="tecnology-icon" />
-                ) : (
-                  <i className={`${item.iconClass} tecnology-icon`}></i>
-                )}
-                <p className="tecnology-name">{item.label}</p>
-              </div>
-            ))}
+        <div className="container row">
+          <div className="col-md-6 section2-text">
+            <h2>Technologies & Tools</h2>
+            <p>
+              Throughout the bootcamp, we explored various backend and frontend technologies while
+              gaining practical experience with databases and development tools.
+            </p>
+            <br></br>
+            <p>
+              To ensure an organized and efficient workflow, we used Trello for task management and
+              GitHub for collaboration across three defined sprints. Discord served as our main
+              communication channel, helping us coordinate effectively as a team.
+            </p>
+            <p>
+              These tools played a key role in enhancing our productivity and maintaining a
+              structured development process.
+            </p>
+          </div>
+          <div className="col-md-6 icons">
+            <div className="technologies-row d-flex flex-wrap">
+              {techTools.map((item, index) => (
+                <div className="tecnology-item col-3 text-center py-4" key={index}>
+                  {item.isCustomIcon ? (
+                    <SupabaseIcon className="tecnology-icon" />
+                  ) : (
+                    <i className={`${item.iconClass} tecnology-icon`}></i>
+                  )}
+                  <p className="tecnology-name">{item.label}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
 
       <div className="project-section">
-        <div className="section3">
+        <div className="section3 container">
           <div className="section3-text">
             <h2>Project Organization - MER</h2>
             <p>
@@ -139,69 +141,68 @@ const About = () => {
 
       <div className="dashboard-section">
         <div className="section4 text-center">
-          <h2>Admin Dashboard</h2>
-          <p>
-            This e-commerce platform features an intuitive admin dashboard that allows users to
-            manage products, categories, and customers by adding, editing, or deleting items.
-            Additionally, administrators can track order statuses and make any necessary updates or
-            adjustments.
-          </p>
-          <img
-            src="/img/dashboardImage.png"
-            alt="Admin Image"
-            className="admin-image pt-3"
-          />
-          <div className="content-wrapper">
-            <div className="left-column">
-              <p>To test our Admin dashboard, follow these steps:</p>
-              <ol className="text-left">
-                <li>
-                  Visit our{" "}
-                  <Link to="http://localhost:5174/admin/login" className="login-link">
-                    admin login page
-                  </Link>
-                </li>
-                <li>
-                  Use the email address <strong>"admin@homedeluxe.com"</strong> as username.
-                </li>
-                <li>
-                  Input <strong>"password123"</strong> as the password.
-                </li>
-                <li>Click on the login button or submit the form.</li>
-              </ol>
+          <div className="container">
+            <h2>Admin Dashboard</h2>
+            <p className="mx-auto text-center w-75 w-md-50">
+              This e-commerce platform features an intuitive admin dashboard that allows users to
+              manage products, categories, and customers by adding, editing, or deleting items.
+              Additionally, administrators can track order statuses and make any necessary updates
+              or adjustments.
+            </p>
+            <img src="/img/dashboardImage.png" alt="Admin Image" className="admin-image pt-3" />
+
+            <div className="content-wrapper">
+              <div className="left-column">
+                <p>To test our Admin dashboard, follow these steps:</p>
+                <ol className="text-left">
+                  <li>
+                    Visit our{" "}
+                    <Link to="http://localhost:5174/admin/login" className="login-link">
+                      admin login page
+                    </Link>
+                  </li>
+                  <li>
+                    Use the email address <strong>"admin@homedeluxe.com"</strong> as username.
+                  </li>
+                  <li>
+                    Input <strong>"password123"</strong> as the password.
+                  </li>
+                  <li>Click on the login button or submit the form.</li>
+                </ol>
+              </div>
+
+              <div className="right-column">
+                <p>To test the application as a user, follow these steps:</p>
+                <ol className="text-left">
+                  <li>
+                    Visit our{" "}
+                    <Link to="/login" className="login-link">
+                      login page
+                    </Link>
+                  </li>
+                  <li>
+                    Use the email address <strong>"userprueba@gmail.com"</strong> as username.
+                  </li>
+                  <li>
+                    Input <strong>"password123"</strong> as the password.
+                  </li>
+                  <li>Click on the login button or submit the form.</li>
+                </ol>
+              </div>
             </div>
 
-            <div className="right-column">
-              <p>To test the application as a user, follow these steps:</p>
-              <ol className="text-left">
-                <li>
-                  Visit our{" "}
-                  <Link to="/login" className="login-link">
-                    login page
-                  </Link>
-                </li>
-                <li>
-                  Use the email address <strong>"userprueba@gmail.com"</strong> as username.
-                </li>
-                <li>
-                  Input <strong>"password123"</strong> as the password.
-                </li>
-                <li>Click on the login button or submit the form.</li>
-              </ol>
-            </div>
+            <p className="final-note pt-4">
+              Now you can access the admin dashboard and test the application with the provided
+              login data. Enjoy the testing!
+            </p>
           </div>
-
-          <p className="final-note pt-4">
-            Now you can access the admin dashboard and test the application with the provided login
-            data. Enjoy the testing!
-          </p>
         </div>
       </div>
 
       <div className="members-section">
         <div className="section5 text-center">
           <h2>Our Team</h2>
-          <div className="row justify-content-center">
+          <div className="container row justify-content-center">
             {teamMembers.map((member) => (
               <div className="col-md-auto" key={member.id}>
                 <div className="team-member">
